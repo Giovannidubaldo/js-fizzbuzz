cb5ed3371f3851029783a6e23cf7606c306d137a
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-22 16:51:39 +0100

    Added element on html

--- js/script.js
+++ js/script.js
@@ -7,22 +7,30 @@ for(let i=1; i<=100; i++){
     if(i%3 == 0 && i%5 ==0){
         let i = 'FizzBuzz';
         console.log(i);
+        let li = `<li id="item-${i}">${i}</li>`;
+        list.innerHTML += li;
     }
 
     // Punto 2.2 e Punto 3.1
     else if(i%5 == 0){
         let i = 'Buzz';
         console.log(i);
+        let li = `<li id="item-${i}">${i}</li>`;
+        list.innerHTML += li;
     }
     
     // Punto 2.3 e Punto 3.2
     else if(i%3 == 0){
         let i = 'Fizz';
         console.log(i);
+        let li = `<li id="item-${i}">${i}</li>`;
+        list.innerHTML += li;
     }
 
     // Punto 4
     else{
         console.log(i);
+        let li = `<li id="item-${i}">${i}</li>`;
+        list.innerHTML += li;
     }
 }
