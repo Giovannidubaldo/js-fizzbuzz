a5ba64caa5514748df7788d7e2929931b1c75fe2
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-22 17:21:01 +0100

    Added changes to let

--- js/script.js
+++ js/script.js
@@ -5,25 +5,25 @@ for(let i=1; i<=100; i++){
 
     // Punto 2.1 e Punto 3
     if(i%3 == 0 && i%5 ==0){
-        let i = 'FizzBuzz';
-        console.log(i);
-        let li = `<li id="item-${i}" class="green">${i}</li>`;
+        let fizz_buzz = 'FizzBuzz';
+        console.log(fizz_buzz);
+        let li = `<li id="item-${i}" class="green">${fizz_buzz}</li>`;
         list.innerHTML += li;
     }
 
     // Punto 2.2 e Punto 3.1
     else if(i%5 == 0){
-        let i = 'Buzz';
-        console.log(i);
-        let li = `<li id="item-${i}" class="red">${i}</li>`;
+        let buzz = 'Buzz';
+        console.log(buzz);
+        let li = `<li id="item-${i}" class="red">${buzz}</li>`;
         list.innerHTML += li;
     }
     
     // Punto 2.3 e Punto 3.2
     else if(i%3 == 0){
-        let i = 'Fizz';
-        console.log(i);
-        let li = `<li id="item-${i}" class="blue">${i}</li>`;
+        let fizz = 'Fizz';
+        console.log(fizz);
+        let li = `<li id="item-${i}" class="blue">${fizz}</li>`;
         list.innerHTML += li;
     }
 
