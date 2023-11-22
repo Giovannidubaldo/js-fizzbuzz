df351f970cad13c389cb41cbdcc8906524140f5b
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-22 16:20:17 +0100

    Update Correction js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+const list = document.querySelector('ul');
 
 // Punto 1 e Punto 2
 for(let i=1; i<=100; i++){
-    console.log(i);
 
     // Punto 2.1 e Punto 3
     if(i%3 == 0 && i%5 ==0){
@@ -18,6 +18,11 @@ for(let i=1; i<=100; i++){
     // Punto 2.3 e Punto 3.2
     else if(i%3 == 0){
         let i = 'Fizz';
+        console.log(i);
+    }
+
+    // Punto 4
+    else{
         console.log(i);
     }
 }
